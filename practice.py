64d931be89a5bffef7b0f2fd343f598058c9e848
Yhuan Shin <tejima911@gmail.com>
2025-05-08 12:11:31 +0800

    Add functions to check odd/even numbers and friends' status

---
+++ practice.py
@@ -0,0 +1,38 @@
+# check if odd or even numbers
+def checkOddEven():
+    num = 5 
+    if num % 2 == 0:
+        print("Even")
+        return True
+    elif num % 2 == 1:
+        print("Odd")
+        return False
+
+
+
+
+# Check the status
+
+# a = 1
+# b = 1
+# flag = True
+
+# if (a >= 0 != b >= 0) and not flag:
+#     print(True)
+# elif (a < 0 and b < 0) and flag:
+#     print(True)
+# else:
+#     print(False)
+
+def friends_in_trouble(a,b):
+    if a==True and b==True:
+        print("You're in trouble")
+    elif a==True or b ==False:
+        print("Your good")
+
+j_angry = True
+s_angry = True
+
+friends_in_trouble(j_angry, s_angry)
+
+
